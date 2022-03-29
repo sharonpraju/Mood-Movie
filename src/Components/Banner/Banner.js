f08ad856ec91c54ae367c8dbb6f20505bcb65107
Sharon P Raju <sharonpraju@gmail.com>
2022-03-29 15:07:04 +0530

    updates

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,8 +2,25 @@ import React, { useEffect, useState } from 'react'
 import './Banner.css'
 import axios from 'axios';
 import Overview from '../Overview/Overview';
+
+var genres = [];
+var with_genre='';
+axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=5a0394d6b2bf56d802c8239e64c596dc&language=en-US`).then(result => {
+            genres = result.data.genres;
+});
+
+
+
 function Banner() {
     const [banner, setBanner] = useState('');
+    const [genre, setGenre] = useState('');
+
+    function handleGenre()
+    {
+        var temp_genre = `&with_genre=${with_genre}`;
+        setGenre(temp_genre);
+       
+    }
 
     var img_url = `https://image.tmdb.org/t/p/original${banner.backdrop_path}`;
 
@@ -11,7 +28,7 @@ function Banner() {
         //runs when the component is mounted
         var page = Math.floor(Math.random() * (10 - 1 + 1) + 1);
         var x = Math.floor(Math.random() * (10 - 1 + 1) + 1);
-        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=5a0394d6b2bf56d802c8239e64c596dc&sort_by=popularity.desc&page=${page}&with_watch_monetization_types=flatrate`).then(result => {
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=5a0394d6b2bf56d802c8239e64c596dc&sort_by=popularity.desc&page=${page}${genre}&with_watch_monetization_types=flatrate`).then(result => {
             console.log(result.data.results[x]);
             setBanner(result.data.results[x])
         });
@@ -22,18 +39,32 @@ function Banner() {
     return (
         <div>
             <div className='banner' style={{backgroundImage:`url(${banner ? img_url : ''})`}}>
-                <center>
                 <div className='content' >
-                    <h2 >Random Movie Suggestion</h2>
-                    <h1 className='title'>{banner.title}</h1>
-                    <br/>
-                    <br/>
-                    <a className="button">Rating : {banner.vote_average}</a>
+                    <center>
+                        <h2 >Random Movie Suggestion</h2>
+                        <h1 className='title'>{banner.title}</h1>
+                        <br/>
+                        <br/>
+                        <a className="button">Rating : {banner.vote_average}</a>
+                        <br/>
+                        <div className="select_div">
+                            <select className="select" onChange={(e)=>{with_genre=e.target.value
+                             
+                            }}>
+                                {
+                                    genres && genres.map((obj, index)=>{
+                                        return (<option key={index} value={obj.name}>{obj.name}</option>)
+                                    })
+                                }
+                            </select>
+                            <a className="button-x" onClick={handleGenre}>Search</a>
+                        </div>
+                        <br/><br/>
+                        <a className="button-x">Get Random Movie</a>
+                    </center>
+                    <Overview overview={banner.overview}></Overview>
                 </div>
-                </center>
-            <div className="fade_bottom">
-                <Overview overview={banner.overview}></Overview>
-            </div>
+                <div className="fade_bottom"></div>
             </div>
         </div>
     )
